5aa8ed726878dc91fc847080939bd1dc86a2a601
Tomáš Trachta <TTrachta@spsdomain.local>
2020-12-11 08:01:26 +0100

    ..

--- form.php
+++ form.php
@@ -6,7 +6,7 @@
 
     <div id="form">
         <?php 
-        if(isset($_GET['Jmeno']) || isset($_GET['Prijmeni']) || isset($_GET['Datum']) || isset($_GET['Vyska']) || isset($_GET['Barva'])){        
+        if(!empty($_GET)){        
         echo "<h1>GET</h1><br>
         Vaše jméno: ". $_GET['Jmeno'] ."<br>";
         echo "Vaše Příjmení: ". $_GET['Prijmeni'] ."<br>";
@@ -15,10 +15,11 @@
         echo "Vaše oblíbená barva: ". $_GET['Barva'] ."<br><br>";      
 
             foreach($_GET as $key => $value){
+                if(!empty($value) || ctype_digit($value))
                 echo "GET parametr '$key' má hodnotu '$value'<br>";
             }  
         }
-        if(isset($_POST['Jmeno']) || isset($_POST['Prijmeni']) || isset($_POST['Datum']) || isset($_POST['Vyska']) || isset($_POST['Barva'])){        
+        if(!empty($_POST)){        
             echo "<h1>POST</h1><br>
             Vaše jméno: ". $_POST['Jmeno'] ."<br>";
             echo "Vaše Příjmení: ". $_POST['Prijmeni'] ."<br>";
@@ -27,6 +28,7 @@
             echo "Vaše oblíbená barva: ". $_POST['Barva'] ."<br><br>";  
             
             foreach($_POST as $key => $value){
+                if(!empty($value) || ctype_digit($value))
                 echo "POST parametr '$key' má hodnotu '$value'<br>";
             } 
         }
@@ -35,17 +37,18 @@
         echo "Vaše Příjmení: ". $_REQUEST['Prijmeni'] ."<br>";
         echo "Vaše datum narození: ". $_REQUEST['Datum'] ."<br>";
         echo "Vaše výška: ". $_REQUEST['Vyska'] ."<br>";
-        echo "Vaše oblíbená barva: ". $_REQUEST['Barva'] ."<br><br>";
+        echo "Vaše oblíbená barva: ". $_REQUEST['Barva'] ."<br><br><br>";
 
         
         if(isset($_GET['Jmeno'])){
+            echo "<a href=\"?Jmeno=1\">Zadat 1 do pole GET</a>";
             $val = intval($_GET['Jmeno']);
             echo "<div class=\"table\">";
             echo "<table>";        
             for($j = 0; $j < 3; $j++){
                 echo "<tr>";
                 for($k = 0; $k < 5; $k++){
-                    if(ctype_digit($_GET['Jmeno'])){
+                    if(is_numeric($_GET['Jmeno'])){
                         echo "<td>{$val}</td>";
                         $val++;
                     }
@@ -57,6 +60,6 @@
             echo "</table>";
             echo "</div>";
         }
-        ?>
+        ?>        
     </div>
     <?php require_once('foot.php'); ?>
